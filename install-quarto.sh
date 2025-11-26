2537719b0ceb4e190d76079f6819b58b6f18729d
EvergreenCannaLedger <franklin@evergreencannaledger.org>
2025-11-26 12:04:25 -0800

    Ensure Quarto install script is executable

--- install-quarto.sh
+++ install-quarto.sh
@@ -6,8 +6,8 @@ echo "Installing Quarto version ${QUARTO_VERSION}..."
 
 wget -q https://quarto.org/download/latest/quarto-linux-amd64.tar.gz
 tar -xzf quarto-linux-amd64.tar.gz
-sudo mv quarto-* /opt/quarto
-sudo ln -s /opt/quarto/bin/quarto /usr/local/bin/quarto
+mv quarto-* /opt/quarto
+ln -s /opt/quarto/bin/quarto /usr/local/bin/quarto
 
 echo "Quarto installed:"
 quarto --version
